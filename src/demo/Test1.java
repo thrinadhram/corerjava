273dee47cae7e64ab2ae198ad6fcee79c2e792f9
thrinadh <thrinadhram00206@gmail.com>
2022-08-24 12:49:48 +0530

    "I have updated Test1 file"

--- src/demo/Test1.java
+++ src/demo/Test1.java
@@ -3,8 +3,9 @@ package demo;
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
    System.out.println("hi");
+   System.out.println("sushma REDDY");
 	}
 
 }
